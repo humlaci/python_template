a1e2f08950000bc1d3a7f25ed6b69e236d4f87f1
humlaci <73293782+humlaci@users.noreply.github.com>
2025-04-14 21:08:47 +0200

    Update main.py
    logger import modifications

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 """Module docstring."""
 
 # Imports
-import my_logger
+from my_logger import logger
 from dotenv import dotenv_values
 from dotenv import load_dotenv
 
@@ -19,11 +19,11 @@ load_dotenv()
 
 def main(*args):
     """Program execution starts here."""
-    my_logger.logger.debug('This is a debug message')
-    my_logger.logger.info('This is an info message')
-    my_logger.logger.warning('This is a warning message')
-    my_logger.logger.error('This is an error message')
-    my_logger.logger.critical('This is a critical message')
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
     main()
